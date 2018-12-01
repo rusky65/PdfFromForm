e5ce0df09d170354327bf63e24fdd62e659d5884
rusky65 <->
2018-12-01 09:47:51 +0100

    #3: Creating model and view for  Personal details page (class PersonalDetails, class PersonalDetailsVM). Data binding.

---
+++ PdfFromForm/Models/PersonalDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfFromForm.Models
+{
+    public enum GenderType
+    {
+        male,
+        female
+    }
+
+    class PersonalDetails
+    {
+        public int PersonalId { get; set; }
+        private GenderType gender;
+
+        public GenderType Gender {
+            get { return gender; }
+            set {
+                gender = value; }
+        }
+
+        //public GenderType Gender { get; set; }
+        public string PrefixTitle { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Nationality { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MobilPhone { get; set; }
+        public string EmailAddress { get; set; }
+        public string WebPages { get; set; }
+        public int PostCode { get; set; }
+        public string PostCity { get; set; }
+        public string StreetAndHouseNumber { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ PdfFromForm/ViewModels/PersonalDetailsVM.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfFromForm.Models;
+using System.ComponentModel;
+
+namespace PdfFromForm.ViewModels
+{
+    class PersonalDetailsVM : INotifyPropertyChanged
+    {
+        List<PersonalDetails> perssonalDetailsList = new List<PersonalDetails>();
+
+        public PersonalDetailsVM()
+        {
+            perssonalDetailsList.Clear();
+            perssonalDetailsList.Add(new PersonalDetails
+                {
+                    PersonalId = 1,
+                    Gender = GenderType.female,
+                    PrefixTitle = "Dr.",
+                    FirstName = "testFirstName",
+                    LastName = "testLastName",
+                    BirthDate = DateTime.Parse("10.10.1995"),
+                    Nationality = "testNationality",
+                    PhoneNumber = "+15 1111 333 887",
+                    MobilPhone = "+15 1111 333 774",
+                    EmailAddress = "test@test.test",
+                    WebPages = "www.testWebPage.page",
+                    PostCode = 88255,
+                    PostCity = "testCity",
+                    StreetAndHouseNumber = "testStreet 115./G",
+                    Country = "testCountry"
+                });
+        }
+
+        private int _personId;
+
+        public int PersonId {
+            get { return _personId; }
+            set {
+                _personId = value;
+                RaisePropertyChanged("PersonId");
+                GetPersonalDetails(_personId);
+            }
+        }
+
+
+        private PersonalDetails searchedPersonalDetails = new PersonalDetails();
+        public PersonalDetails SearchedPersonalDetails {
+
+            get { return searchedPersonalDetails; }
+
+            set {
+                searchedPersonalDetails = value;
+                // It fill the details of PersonalDetails
+                RaisePropertyChanged("SearchedPersonalDetails");
+            }
+        }
+
+        /// <summary>
+        /// Find and get the PersonalDetails class for personId parameter.
+        /// </summary>
+        /// <param name="personId"></param>
+        private void GetPersonalDetails(int _personId)
+        {
+            SearchedPersonalDetails = perssonalDetailsList.Where(x => x.PersonalId == _personId).Single();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
